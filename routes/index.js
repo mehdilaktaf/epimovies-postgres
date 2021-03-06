ebdae1171ec7029aba17c12edb92b5edc3dbe121
mehdilaktaf <mehdi.laktaf@gmail.com>
2021-03-06 01:12:46 +0100

    User test routes added

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,13 @@ const movies = require('./movies')
 const users = require('./users')
 const auth = require('./auth')
 module.exports = app => {
+  app.use(function(req, res, next) {
+    res.header(
+      "Access-Control-Allow-Headers",
+      "x-access-token, Origin, Content-Type, Accept"
+    );
+    next();
+  });
   app.use('/movies', movies)
   app.use('/users', users)
   app.use('/auth', auth)
--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 const Router = require('express-promise-router')
-const { verifySignUp } = require("../middleware");
+const { verifySignUp, authJwt } = require("../middleware");
 const settings = require('../settings');
-const controller = require("../controllers/auth.controller");
+const controller = require("../controllers/user.controller");
 
 // create a new express-promise-router
 // this has the same API as the normal express router except
@@ -12,10 +12,11 @@ module.exports = router
 
 // ROUTES //
 // create a user 
-router.post('/', async (req, res) => {
-    console.log('Creating user...')
-    res.status(200).send('User created!');
-})
+router.get(
+    '/', 
+    [authJwt.verifyToken],
+    controller.userBoard
+    )
   
 // get all users
 
