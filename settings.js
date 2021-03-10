e2278d683388752c86a559a48e7dea38977e73d2
mehdilaktaf <mehdi.laktaf@gmail.com>
2021-03-10 18:48:23 +0100

    Set up heroku deployment

--- settings.js
+++ settings.js
@@ -1,10 +1,10 @@
 
 module.exports = Object.freeze({
-    postgres_user: 'postgres',
-    postgres_pwd: 'Bounana1!',
-    postgres_db: 'epimovies_psql',
-    postgres_port: 5432,
-    postgres_host: 'localhost',
+    postgres_user: 'postgres' || process.env.POSTGRESQL_ADDON_USER,
+    postgres_pwd: 'Bounana1!' || process.env.POSTGRESQL_ADDON_PASSWORD,
+    postgres_db: 'epimovies_psql' || process.env.POSTGRESQL_ADDON_DB,
+    postgres_port: 5432 || process.env.POSTGRESQL_ADDON_PORT,
+    postgres_host: 'localhost' || process.env.POSTGRESQL_ADDON_HOST,
     server_port: 5000,
     roles: ["user", "admin", "moderator"],
     secret: "epimovies-secret-key"
